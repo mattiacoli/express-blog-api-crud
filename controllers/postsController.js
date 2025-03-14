73cfd7194afe57727ca05325939789437dfba833
mattiacoli <mattia.coli@aol.com>
2025-03-14 15:47:38 +0100

    edit response on routers index, show , destroy

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,32 +1,47 @@
+const { log } = require('console')
 const posts = require('../data/posts')
 
 // index
-function index(req, res){
-  res.send('post list here')
+function index(req, res) {
+  res.json(posts)
 }
 // show
-function show(req, res){
+function show(req, res) {
   const postSlug = req.params.slug
-  res.send(`this is post with slug:${postSlug}`)
+  const post = posts.find(post => post.slug === postSlug)
+  if(!post){
+    res.send('post not found')
+  }
+  res.json(post)
 }
 // store
-function store(req, res){
+function store(req, res) {
   res.send('add new post')
 }
 // update
-function update(req, res){
+function update(req, res) {
   const postSlug = req.params.slug
   res.send(`update entire post with slug:${postSlug}`)
 }
 // modify
-function modify(req, res){
+function modify(req, res) {
   const postSlug = req.params.slug
   res.send(`modify post with slug:${postSlug}`)
 }
 // destroy
-function destroy(req, res){
+function destroy(req, res) {
+
   const postSlug = req.params.slug
-  res.send(`delete post with slug:${postSlug}`)
+  const post = posts.find(post => post.slug === postSlug)
+  if(!post){
+    res.send('post not found')
+  }
+
+  posts.splice(posts.indexOf(post), 1)
+  console.log(posts);
+
+  res.send(`deleted post with slug ${postSlug}`)
+
 }
 
-module.exports = { index, show, store, update, modify, destroy}
+module.exports = { index, show, store, update, modify, destroy }
